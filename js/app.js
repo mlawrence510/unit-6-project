1db8c307019345c78055a68975325e7640733ebe
mlawrence510 <66187887+mlawrence510@users.noreply.github.com>
2020-07-21 19:46:01 -0400

    got the letters to display

--- js/app.js
+++ js/app.js
@@ -8,33 +8,34 @@ const ul = phrase.children[0];
 const missed = 0;
 const phrases = ['Hello World', 'Funeralopolis', 'Sour Cream and Onion', 'Fox Creek', 'Javascript is fun'];
 
-const getRandomPhraseAsArray = arr => {
+const getRandomPhraseToDisplay = (arr) => {
   const index = Math.floor(Math.random() * arr.length)
   const random = arr[index];
-  return random;
-}
-
-const addPhraseToDisplay = str => {
-  let letters = Array.from(str);
+  let letters = Array.from(random);
   return letters;
 }
 
-function createLetters (arr) {
-  console.log(arr);
-  for (i = 0; i < arr.length; i++);
-  const letter = arr[i];
-  let text = letter;
-  const p = document.createElement('p');
-  const li = document.createElement('li');
-  p.className = 'letter show';
-  p.textContent = text;
-  li.appendChild(p);
-  return li;
-}
+
+const addPhrasetoDisplay = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = arr[i];
+
+    if (arr[i] === " ") {
+      li.className = "space";
+    } else {
+      li.className = "letter";
+    }
+
+    ul.appendChild(li);
+  }
+};
+
+
 
 
 resetButton.addEventListener('click', () => {
   overlay.style.display = 'none';
-  const li = createLetters(addPhraseToDisplay(getRandomPhraseAsArray(phrases)));
-  ul.appendChild(li);
+  const phrase = getRandomPhraseToDisplay(phrases);
+  addPhrasetoDisplay(phrase);
 });
